0ac750639180583ed59522d7e6e5500cd3668811
RakicSrdjan <37338933+RakicSrdjan@users.noreply.github.com>
2020-07-11 23:47:23 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { ToolbarService, DocumentEditorComponent, DocumentEditorContainerComponent } from '@syncfusion/ej2-angular-documenteditor';
+import { ToolbarService, DocumentEditorComponent, DocumentEditorContainerComponent, EditorService, SelectionService, SfdtExportService, TextExportService } from '@syncfusion/ej2-angular-documenteditor';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [ToolbarService]
+  providers: [ToolbarService,EditorService, SelectionService, SfdtExportService,TextExportService]
 })
 export class AppComponent implements OnInit {
   @ViewChild('documentEditorDefault', { static: true }) 
